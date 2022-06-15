1bafd4956cae0c75c6046e1daa2fb350b1c80252
Jessica Brochu <jesbrochu@gmail.com>
2022-06-15 09:52:51 -0400

    feature: header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper">
+    <nav>
+      <div class="logo">
+        <a href="#"><img src="../src/assets/logo.svg" alt="logo"></a>
+      </div>
+      <div class="navigation">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/contact">Contact</router-link>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Inter', sans-serif;
+  font-size: 16px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #2A2B30;
+}
+
+.wrapper{
+  max-width: 1440px;
+  width: 100%;
+  margin: 0 auto;
 }
 
 nav {
-  padding: 30px;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  padding-right: 50px;
+  font-weight: 400;
+  color: #2A2B30;
+  text-decoration: none;
+
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #2A2B30;
 }
 </style>
